54d1a85e2c71a3a415b5180920fdcd1bbd4476e2
aymen maiziz <aymenmaiziz@aymens-MacBook-Pro.local>
2023-04-26 15:58:35 +0100

    first

---
+++ hello.py
@@ -0,0 +1,6 @@
+def more_hello():
+    return "Hello"
+
+def hello_world():
+    return "Hello World"
+
---
+++ test_hello.py
@@ -0,0 +1,8 @@
+from hello import *
+def test_more_hello():
+    assert more_hello() == "Hello"
+
+def test_hello_world():
+    assert hello_world() == "Hello World"
+
+
